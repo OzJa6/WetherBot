e90e7e144c42e8e24f115144664ddbbda3ec983a
OzJa6 <mihail-romashov01@yandex.ru>
2022-04-07 15:29:31 +0500

    after NuGet

---
+++ Parser.cs
@@ -0,0 +1,12 @@
+using AngleSharp;
+
+namespace Parser // Note: actual namespace depends on the project name.
+{
+    public class Parser
+    {
+        public static void parse()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
